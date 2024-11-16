05d7c580944ea29fd9b018a77a060d8cbef91f22
conta idriss <idriss1elouiri2@gmail.com>
2024-11-16 10:14:43 +0100

    save changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,16 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/carWEB.png";
-import projImg2 from "../assets/img/roof-image.png";
-import projImg3 from "../assets/img/screencapture-idrisss-car-site-87a306-webflow-io-2024-11-03-08_21_54.png";
-import projImg4 from "../assets/img/screencapture-idrisss-five-star-site-b590c7-webflow-io-2024-11-03-09_11_04.png";
-import projImg5 from "../assets/img/ecommerce-image.png";
-import projImg6 from "../assets/img/reatEstate.png";
-import projImg7 from "../assets/img/dashbaord.png";
-import projImg8 from "../assets/img/Pos System Restaurent.png";
+import projImg1 from "../assets/img/car-des.png";
+import projImg2 from "../assets/img/roof-des.png";
+import projImg3 from "../assets/img/car-web.png";
+import projImg4 from "../assets/img/real-web.png";
+import projImg5 from "../assets/img/ecom-web.png";
+import projImg6 from "../assets/img/ecom-code.png";
+import projImg7 from "../assets/img/real-code.png";
+import projImg8 from "../assets/img/dashboard-ecom.png";
+import projImg9 from "../assets/img/pos-system-restaurent.png";
+import projImg10 from "../assets/img/car-rental-system.png";
+
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
@@ -40,6 +43,13 @@ export const Projects = () => {
       alt: "image",
       linkWeb: "https://idrisss-five-star-site-b590c7.webflow.io/",
       description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      titleVistitWeb: "Visit website",
+      alt: "image",
+      linkWeb: "https://idrisss-car-site-87a306.webflow.io/",
+      description: "Design & Development",
       imgUrl: projImg4,
     },
     {
@@ -47,7 +57,7 @@ export const Projects = () => {
       alt: "image",
       linkWeb: "https://idrisss-car-site-87a306.webflow.io/",
       description: "Design & Development",
-      imgUrl: projImg3,
+      imgUrl: projImg5,
     },
   ];
   const projects3 = [
@@ -55,34 +65,45 @@ export const Projects = () => {
       titleVistitWeb: "Visit website",
       titleVistitCode: "Visit code",
       alt: "image",
-      linkWeb: "https://real-estate-five-xi.vercel.app/",
-      linkCode: "https://github.com/idriss-elouiri/mern-realEstate",
+      linkWeb: "https://mern-ecommerceweb.onrender.com",
+      linkCode: "https://github.com/idriss-elouiri/mern-ecommerceWeb",
       description: "Design & Development",
-      imgUrl: projImg5,
+      imgUrl: projImg6,
     },
     {
       titleVistitWeb: "Visit website",
       titleVistitCode: "Visit code",
       alt: "image",
-      linkCode: "https://github.com/idriss-elouiri/mern-ecommerceWeb",
+      linkWeb: "https://realestate-n2sb.onrender.com",
+      linkCode: "https://github.com/idriss-elouiri/mern-realEstate",
       description: "Design & Development",
-      imgUrl: projImg6,
+      imgUrl: projImg7,
     },
   ];
   const projects4 = [
     {
       titleVistitCode: "Visit code",
       alt: "image",
+      linkWeb: "https://mern-ecommerceweb.onrender.com",
       linkCode: "https://github.com/idriss-elouiri/mern-ecommerceWeb",
       description: "Design & Development",
-      imgUrl: projImg7,
+      imgUrl: projImg8,
     },
     {
       titleVistitCode: "Visit code",
       alt: "image",
+      linkWeb: "https://pos-system-restaurent.vercel.app",
       linkCode: "https://github.com/idriss-elouiri/pos-system-restaurent",
       description: "Design & Development",
-      imgUrl: projImg8,
+      imgUrl: projImg9,
+    },
+    {
+      titleVistitCode: "Visit code",
+      alt: "image",
+      linkWeb: "https://car-rental-system-8lek.vercel.app",
+      linkCode: "https://github.com/idriss-elouiri/Car-Rental-System-",
+      description: "Design & Development",
+      imgUrl: projImg10,
     },
   ];
 
